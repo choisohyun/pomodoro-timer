ce8b58e863e1dd76798505b3f09a065c05c74b63
choisohyun <sohyun4283@naver.com>
2022-04-03 17:57:28 +0900

    feat: add pomodoro timer

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,93 @@
+import { useMemo, useState } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.css';
+import Timer from './Timer';
+import { useTimer } from '../hooks';
 
-const Hello = () => {
+const timer = {
+  work: 55,
+  shortBreak: 5,
+  longBreak: 10,
+  stop: 0,
+};
+const RADIUS = 54;
+const CIRCUMFERENCE = 2 * Math.PI * RADIUS;
+
+type Mode = 'work' | 'shortBreak' | 'longBreak' | 'stop';
+
+const TimerWrapper = () => {
+  const [mode, setMode] = useState<Mode>('work');
+  const [active, setActive] = useState(false);
+  const remainTime = useTimer(timer[mode] * 60, active);
+
+  const dashoffset = useMemo(() => {
+    const process = remainTime / (timer[mode] * 60);
+    return CIRCUMFERENCE * (1 - process);
+  }, [mode, remainTime]);
+
+  const handleMode = (selectedMode: Mode) => {
+    if (selectedMode === 'stop') {
+      setActive((prev) => !prev);
+    } else {
+      setMode(selectedMode);
+      setActive(true);
+    }
+  };
+
   return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
+    <div className="wrapper">
+      <h1>Pomodoro Timer</h1>
+      <div className="container">
+        <div className="buttons">
+          <button onClick={() => handleMode('work')} type="button">
+            Work
           </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
+          <button
+            onClick={() => handleMode('shortBreak')}
+            className="shortBreak"
+            type="button"
+          >
+            Short Break
           </button>
-        </a>
+          <button
+            onClick={() => handleMode('longBreak')}
+            className="longBreak"
+            type="button"
+          >
+            Long Break
+          </button>
+          <button
+            onClick={() => handleMode('stop')}
+            className="stop"
+            type="button"
+          >
+            {active ? 'Stop' : 'Start'}
+          </button>
+        </div>
+        <div className="circle_progress_wrap timer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="circle_progress"
+            width="200"
+            height="200"
+            viewBox="0 0 120 120"
+          >
+            <circle className="frame" cx="60" cy="60" r="54" strokeWidth="12" />
+            <circle
+              className="bar"
+              cx="60"
+              cy="60"
+              r="54"
+              strokeWidth="12"
+              strokeDashoffset={dashoffset}
+              strokeDasharray={CIRCUMFERENCE}
+            />
+          </svg>
+          <div className="value">
+            <Timer time={timer[mode] * 60} active={active} />
+          </div>
+          <div className="filler" />
+        </div>
       </div>
     </div>
   );
@@ -43,7 +97,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<TimerWrapper />} />
       </Routes>
     </Router>
   );
